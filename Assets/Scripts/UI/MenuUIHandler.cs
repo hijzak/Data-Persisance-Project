4c1a1970f422c7b5304eb514bc2c37717cf3eedc
hijzak <124872465+hijzak@users.noreply.github.com>
2023-03-01 14:25:31 +0900

    V_1.0
    メニュー画面でスコアを表示するようにした。

--- Assets/Scripts/UI/MenuUIHandler.cs
+++ Assets/Scripts/UI/MenuUIHandler.cs
@@ -5,15 +5,26 @@ using System;
 using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MenuUIHandler : MonoBehaviour
 {
     public TMP_InputField nameInput;
+    public TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!PlayerPrefs.HasKey("HighScore"))
+        {
+            scoreText.text = "Best Score";
+        }
+        else
+        {
+            scoreText.text = "Best Score : " + PlayerPrefs.GetString("HighScoreName") + " : " + PlayerPrefs.GetInt("HighScore");
+        }
     }
 
     public void SaveName()
@@ -25,6 +36,15 @@ public class MenuUIHandler : MonoBehaviour
     public void StartNew()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 
     // Update is called once per frame
